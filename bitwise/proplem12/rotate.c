fbb275c61bbfb3ec46ae9e06836c9c33f8d707b3
salma mohamed <salmafouadothman@gamil.com>
2026-08-12 07:33:20 +0300

    bitwis tasks

--- bitwise/proplem12/rotate.c
+++ bitwise/proplem12/rotate.c
@@ -1,8 +1,8 @@
 #include"header.h"
 void rotation(uint8_t number,uint8_t rotation_number){
     uint8_t rotated_number=0;
-    rotated_number=(number<<rotation_number)|number;
+    rotated_number=(number<<rotation_number);
     printf("left : %hhd\n",rotated_number);
-    rotated_number=(number>>rotation_number)|number;
+    rotated_number=(number>>rotation_number);
     printf("right : %hhd",rotated_number);
 }
